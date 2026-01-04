80268fa49fda9ef1d93222c8c4a9ca3212c63145
Sjain <sjain23062000@gmail.com>
2026-01-04 12:45:28 +0530

    Insertion sort

---
+++ DSA/src/Sorting/InsertionSort.java
@@ -0,0 +1,27 @@
+package Sorting;
+
+import java.util.Scanner;
+
+public class InsertionSort {
+    public static void main(String[] args)
+    {
+        int arr[] = new int[5];
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < arr.length; i++) {
+            arr[i]= sc.nextInt();
+        }
+        System.out.println("----------------------Insertion Sort----------------------");
+        for (int i = 1; i < arr.length; i++) {
+        int key=arr[i];
+        int j=i-1;
+        while(j>=0 && arr[j]>key)
+        {
+        arr[j+1]=arr[j];
+        j--;
+        }
+        arr[j+1]=key;
+        }
+        for (int x:arr)
+            System.out.println(x);
+    }
+}
